73c36561043c278e31981938986b7787966a14c6
thirteenpylons <thirteenpylons@gmail.com>
2021-12-12 21:21:42 -0500

    done

--- basic/string1.py
+++ basic/string1.py
@@ -33,8 +33,7 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  return "" if len(s) < 2 else f"{s[:2]}{s[-2:]}"
 
 
 # C. fix_start
