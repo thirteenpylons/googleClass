96e930e483052ef6b0dcfb8dc5aeeac98d94293a
thirteenpylons <thirteenpylons@gmail.com>
2021-12-17 09:36:30 -0500

    mix_up complete

--- basic/string1.py
+++ basic/string1.py
@@ -56,8 +56,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  return a.replace(a[:2], b[:2]) + " " + b.replace(b[:2], a[:2])
 
 
 # Provided simple test() function used in main() to print
